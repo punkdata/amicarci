45d0b49535dafac93d997664577431188d8a4138
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-01 09:48:36 -0400

    a
    <

--- Amicar/build/setup-prod.sh
+++ Amicar/build/setup-prod.sh
@@ -1,8 +1,9 @@
 # Get the private key from the environment variable
 echo "Setting up Production Connection..."
 mkdir keys
-echo $SFDX_CONSUMER_KEY | base64 -d > keys/server.key
+echo $SFDX_JWT_KEY | base64 -d > keys/server.key
 
 # Authenticate to salesforce
 echo "Authenticating..."
-sfdx force:auth:jwt:grant --clientid $SFDX_JWT_KEY --jwtkeyfile keys/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a DevHub
+sfdx force:auth:jwt:grant --clientid $SFDX_CONSUMER_KEY --jwtkeyfile keys/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a DevHub
+ 
